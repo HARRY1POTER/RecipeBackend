0db62ceefa1eb177b1aff18aeaba3851ac2374d6
Anirudhsinh <mrhpotter124@gmail.com>
2024-07-26 12:16:30 +0530

    .

--- Middleware/upload.js
+++ Middleware/upload.js
@@ -3,8 +3,8 @@ const multer = require("multer");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // cb(null, "uploads/");
-    // cb(null, "/tmp");
-    cb(null, "/upload");
+    cb(null, "/tmp");
+    // cb(null, "/upload");
 
     
 
